fb366ddbe45a02e41e09e0ef0aa255326928540c
Kevan Fisher <kfisher@freshbooks.com>
2016-09-28 15:55:39 -0400

    Switch from auto injection to manual.
    The media service both conflicts with service names in other addons and causes unnecessary overhead when injected on everything. Moving forward manual injection should be preferred. If auto injection is still required, initializers can be made for the specific application.

--- addon/initializers/responsive.js
+++ addon/initializers/responsive.js
@@ -1,14 +1,30 @@
+import Ember from 'ember';
+
+const { deprecate, getOwner } = Ember;
+const AUTO_INJECTION_DEPRECATION_MESSAGE = '[ember-responsive] Future versions of ember-responsive will no longer inject the media service automatically. Instead you should explicitly inject it into your Route, Controller or Component with `Ember.inject.service`.';
+
 /**
  * Ember responsive initializer,
  *
  * Injects the media service in all controllers route components and views
  */
-export function initialize() {
-  let application = arguments[1] || arguments[0];
-  application.inject('controller', 'media', 'service:media');
-  application.inject('component', 'media', 'service:media');
-  application.inject('route', 'media', 'service:media');
-  application.inject('view', 'media', 'service:media');
+export function initialize(application) {
+  const config = getOwner(application).resolveRegistration('config:environment');
+  const { emberResponsiveDefaults: { autoInject }} = config;
+  const shouldShowDeprecation = autoInject;
+
+  getOwner(application).lookup('service:responsive').init();
+
+  deprecate(AUTO_INJECTION_DEPRECATION_MESSAGE, shouldShowDeprecation, {
+    id: 'ember-responsive.deprecate-auto-injection'
+  });
+
+  if (autoInject) {
+    application.inject('controller', 'media', 'service:media');
+    application.inject('component', 'media', 'service:media');
+    application.inject('route', 'media', 'service:media');
+    application.inject('view', 'media', 'service:media');
+  }
 }
 
 export default {
