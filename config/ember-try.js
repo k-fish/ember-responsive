d3fd2f5da85cd80fe74cb2283e75b806eedf54fd
Kevan Fisher <kfisher@freshbooks.com>
2018-01-02 15:00:24 -0500

    Update ember-cli to 2.18

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,8 +1,20 @@
 module.exports = {
   scenarios: [
     {
-      name: 'default',
-      dependencies: { }
+      name: 'ember-lts-2.12',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.12.0'
+        }
+      }
+    },
+    {
+      name: 'ember-lts-2.16',
+      npm: {
+        devDependencies: {
+          'ember-source': '~2.16.0'
+        }
+      }
     },
     {
       name: 'ember-release',
